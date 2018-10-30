93865cd582fbcaa0c1523cba13427a51af6ac5fe
yarepka <yarepka01@gmail.com>
2018-10-30 18:54:07 +0300

    Initial project add

---
+++ src/main/java/guru/springframework/dispring/DiSpringApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.dispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiSpringApplication.class, args);
+    }
+}
---
+++ src/test/java/guru/springframework/dispring/DiSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.dispring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiSpringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
